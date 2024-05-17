5095ec7d2aaa0175fbf166219d1d06ebc135eb2d
Floyd Kersemakers <2204860kersemakers@zuyd.nl>
2024-05-17 13:53:52 +0200

    class exercise made

---
+++ JDM_Casus_Blok4/Classes/Exercise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace JDM_Casus_Blok4.Classes
+{
+    internal class Exercise
+    {
+        public int Id { get; set; }
+        public int ExerciseNumber { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int MaxScore { get; set; }
+
+
+        public Exercise(int id, int exerciseNumber, string name, int score, int maxScore) 
+        {
+            Id = id;
+            ExerciseNumber = exerciseNumber;
+            Name = name;
+            Score = score;
+            MaxScore = maxScore;
+        }
+        public Exercise(int exerciseNumber, string name, int score, int maxScore) 
+        {
+            ExerciseNumber = exerciseNumber;
+            Name = name;
+            Score = score;
+            MaxScore = maxScore;
+        }
+    }
+}
