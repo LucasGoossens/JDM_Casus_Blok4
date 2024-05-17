d7f06b3f9cf1e02c50a9bc6ea680732fd1a750cb
LGZUYD <2305329goossens@zuyd.nl>
2024-05-17 13:12:34 +0200

    User abstract class added

---
+++ JDM_Casus_Blok4/Classes/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDM_Casus_Blok4.UserClasses
+{
+    internal abstract class User
+    {
+        public string Id{ get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string userName, string email, string password)
+        {         
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+
+
+        public User(string id, string userName, string email, string password)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+    }
+}
