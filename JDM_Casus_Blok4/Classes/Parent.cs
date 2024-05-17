a839f57b03c53569a1b2f7bbbc023f7cc569a5a7
lorenzo Pineda Olzheim <2304141pinedaolzheim@zuyd.nl>
2024-05-17 14:13:03 +0200

    parent class alweer gemaakt

---
+++ JDM_Casus_Blok4/Classes/Parent.cs
@@ -0,0 +1,31 @@
+﻿using JDM_Casus_Blok4.UserClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDM_Casus_Blok4.Classes
+{
+    internal class Parent : User
+
+    {
+        public List<Child> Children { get; set; }
+
+        // Constructor zonder parameters
+        public Parent()
+        {
+            Children = new List<Child>();
+        }
+
+        // Constructor met parameters
+        public Parent(string id, string userName, string email, string password, List<Child> children)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+            Password = password;
+            Children = children ?? new List<Child>();
+        }
+    }
+}
