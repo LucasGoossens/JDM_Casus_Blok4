3468296bacf5066326d9436a7b1f3141b6c6feaa
lorenzo Pineda Olzheim <2304141pinedaolzheim@zuyd.nl>
2024-06-25 10:16:09 +0200

    dal gevult assesment researcher

--- JDM_Casus_Blok4/DAL/Dal.cs
+++ JDM_Casus_Blok4/DAL/Dal.cs
@@ -1,6 +1,7 @@
 ﻿using JDM_Casus_Blok4.Classes;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,12 @@ namespace JDM_Casus_Blok4.DAL
     public class Dal
     {
         private static readonly Dal _instance = new Dal();
+        public string connStr = "Data Source=LUCAS;Initial Catalog=JDMDatabase;Integrated Security=True;Encrypt=False;";
+
         private Dal()
         {
-            
         }
+
         public static Dal Instance
         {
             get
@@ -23,11 +26,39 @@ namespace JDM_Casus_Blok4.DAL
         }
 
         // Crud Create:
-
-        public void CreateAssessment()
+        public void CreateAssessment(Assessment assessment)
         {
-            // Create a new assessment
+            string query = "INSERT INTO Assessment (CompletionDate, TotalScore, Validated, PatientAge, PatientId) VALUES (@CompletionDate, @TotalScore, @Validated, @PatientAge, @PatientId);";
+
+            DateTime completionDateTime = assessment.Date.ToDateTime(TimeOnly.MinValue);
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CompletionDate", completionDateTime);
+                        command.Parameters.AddWithValue("@TotalScore", assessment.TotalScore);
+                        command.Parameters.AddWithValue("@Validated", assessment.Validated);
+                        command.Parameters.AddWithValue("@PatientAge", assessment.PatientAge);
+                        command.Parameters.AddWithValue("@PatientId", assessment.PatientId);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving Assessment: " + ex.Message);
+            }
         }
+
+
+
+
+
 
         public void CreateExercise()
         {
@@ -41,9 +72,43 @@ namespace JDM_Casus_Blok4.DAL
 
         // Crud Read:
 
-        public void GetAssessments()
+        public List<Assessment> GetAssessments()
         {
-            // Read exercises
+            List<Assessment> assessments = new List<Assessment>();
+            string query = "SELECT * FROM Assessment";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Assessment assessment = new Assessment
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Date = DateOnly.FromDateTime(reader.GetDateTime(1)),
+                                    TotalScore = reader.GetInt32(2),
+                                    Validated = reader.GetBoolean(3),
+                                    PatientAge = reader.GetInt32(4),
+                                    PatientId = reader.GetInt32(5)
+                                };
+                                assessments.Add(assessment);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error fetching Assessments: " + ex.Message);
+            }
+
+            return assessments;
         }
 
         public void GetExercises()
@@ -76,16 +141,41 @@ namespace JDM_Casus_Blok4.DAL
             // Read doctor
         }
 
-        public void GetRearcher()
+        public void GetResearcher()
         {
             // Read rearcher
         }
 
         // Crud Update:
 
-        public void UpdateAssessment()
+        public void UpdateAssessment(Assessment assessment)
         {
-            // Update an assessment
+            string query = "UPDATE Assessment SET CompletionDate = @CompletionDate, TotalScore = @TotalScore, Validated = @Validated, PatientAge = @PatientAge, PatientId = @PatientId WHERE Id = @Id";
+
+            DateTime completionDateTime = assessment.Date.ToDateTime(TimeOnly.MinValue);
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@CompletionDate", completionDateTime);
+                        command.Parameters.AddWithValue("@TotalScore", assessment.TotalScore);
+                        command.Parameters.AddWithValue("@Validated", assessment.Validated);
+                        command.Parameters.AddWithValue("@PatientAge", assessment.PatientAge);
+                        command.Parameters.AddWithValue("@PatientId", assessment.PatientId);
+                        command.Parameters.AddWithValue("@Id", assessment.Id);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error updating Assessment: " + ex.Message);
+            }
         }
 
         public void UpdateExercise()
