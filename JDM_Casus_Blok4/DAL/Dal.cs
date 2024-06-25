7d574b386b58de87ecf7e0f3c93fa7ec244abc70
LGZUYD <2305329goossens@zuyd.nl>
2024-06-25 12:24:13 +0200

    dal

--- JDM_Casus_Blok4/DAL/Dal.cs
+++ JDM_Casus_Blok4/DAL/Dal.cs
@@ -26,9 +26,58 @@ namespace JDM_Casus_Blok4.DAL
         }
 
         // Crud Create:
+
+        public List<Assessment> GetAssessmentsById(int patientId)
+        {
+            string query = "SELECT * FROM Assessment WHERE PatientId = @PatientId;";
+
+            List<Assessment> assessments = new List<Assessment>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@PatientId", patientId);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                System.Diagnostics.Debug.WriteLine(DateOnly.FromDateTime(reader.GetDateTime(1)));
+
+
+                                Assessment assessment = new Assessment();
+                                assessment.Id = (int)reader["Id"];
+                                assessment.Date = DateOnly.FromDateTime((DateTime)reader["CompletionDate"]);
+                                assessment.TotalScore = (int)reader["TotalScore"];
+                                assessment.Validated = (bool)reader["Validated"];
+                                assessment.PatientAge = (int)reader["PatientAge"];
+                                assessment.PatientId = (int)reader["PatientId"];
+
+                                assessment.Exercises = GetExercisesByAssessmentId(assessment.Id);
+
+                                assessments.Add(assessment);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error getting Assessments: " + ex.Message);
+            }
+            return assessments;
+
+        }
+
         public void CreateAssessment(Assessment assessment)
         {
-            string query = "INSERT INTO Assessment (CompletionDate, TotalScore, Validated, PatientAge, PatientId) VALUES (@CompletionDate, @TotalScore, @Validated, @PatientAge, @PatientId);";
+            string query = "INSERT INTO Assessment (CompletionDate, TotalScore, Validated, PatientAge, PatientId) " +
+                           "OUTPUT INSERTED.Id " +
+                           "VALUES (@CompletionDate, @TotalScore, @Validated, @PatientAge, @PatientId);";
 
             DateTime completionDateTime = assessment.Date.ToDateTime(TimeOnly.MinValue);
 
@@ -45,7 +94,14 @@ namespace JDM_Casus_Blok4.DAL
                         command.Parameters.AddWithValue("@PatientAge", assessment.PatientAge);
                         command.Parameters.AddWithValue("@PatientId", assessment.PatientId);
 
-                        command.ExecuteNonQuery();
+                        int newId = (int)command.ExecuteScalar();
+                        System.Diagnostics.Debug.WriteLine("INSERT ID");
+                        System.Diagnostics.Debug.WriteLine(newId);
+
+                        // weet niet of dit nodig is maar kan handig zijn
+                        assessment.Id = newId;
+
+                        CreateExercise(assessment.Exercises, newId);
                     }
                 }
             }
@@ -56,14 +112,76 @@ namespace JDM_Casus_Blok4.DAL
         }
 
 
-
-
-
-
-        public void CreateExercise()
+        public void CreateExercise(List<Exercise> exercises, int assessmentId)
         {
-            // Create a new exercise
+            string query = "INSERT INTO Exercise (ExerciseNumber, Name, Score, MaxScore, AssessmentId) VALUES (@ExerciseNumber, @Name, @Score, @MaxScore, @AssessmentId);";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        foreach (Exercise exercise in exercises)
+                        {
+                            command.Parameters.Clear();
+                            command.Parameters.AddWithValue("@ExerciseNumber", exercise.ExerciseNumber);
+                            command.Parameters.AddWithValue("@Name", exercise.Name);
+                            command.Parameters.AddWithValue("@Score", exercise.Score);
+                            command.Parameters.AddWithValue("@MaxScore", exercise.MaxScore);
+                            command.Parameters.AddWithValue("@AssessmentId", assessmentId);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving Exercise: " + ex.Message);
+            }
         }
+
+        public List<Exercise> GetExercisesByAssessmentId(int id) {
+            string query = "SELECT * FROM Exercise WHERE AssessmentId = @AssessmentId;";
+
+            List<Exercise> exercises = new List<Exercise>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AssessmentId", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Exercise exercise = new Exercise();
+                                exercise.Id = (int)reader["Id"];
+                                exercise.ExerciseNumber = (int)reader["ExerciseNumber"];
+                                exercise.Name = (string)reader["Name"];
+                                exercise.Score = (int)reader["Score"];
+                                exercise.MaxScore = (int)reader["MaxScore"];
+
+                                exercises.Add(exercise);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error getting Exercises: " + ex.Message);
+            }
+            return exercises;
+        }
+
+
 
         public void CreateFeedback()
         {
